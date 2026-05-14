b4d5ea3a3e350f1e7d1bdb6d7312a5b5eff7163c
carlosmehl <carlosmehlcontato@gmail.com>
2026-05-13 23:29:31 -0300

    Initial commit - Teste de querys Delphi

---
+++ UnitPrincipal.pas
@@ -0,0 +1,65 @@
+unit UnitPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Data.DB,
+  Vcl.Grids, Vcl.DBGrids;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    opcoes: TRadioGroup;
+    txtConsulta: TEdit;
+    lblConsulta: TLabel;
+    BtRealizarConsulta: TButton;
+    DBGrid1: TDBGrid;
+    procedure opcoesClick(Sender: TObject);
+    procedure BtRealizarConsultaClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+uses UnitDM;
+
+procedure TForm1.BtRealizarConsultaClick(Sender: TObject);
+begin
+  DM.sqlConsulta.Close;
+  DM.sqlConsulta.SQL.Clear;
+  if (opcoes.ItemIndex = 0) then
+    begin
+      DM.sqlConsulta.SQL.Add('SELECT * FROM clientesteste WHERE nome LIKE :pConsulta');
+      DM.sqlConsulta.ParamByName('pConsulta').AsString := txtConsulta.Text+'%';
+    end;
+  if (opcoes.ItemIndex = 1) then
+     begin
+      DM.sqlConsulta.SQL.Add('SELECT * FROM clientesteste WHERE bairro LIKE :pConsulta');
+      DM.sqlConsulta.ParamByName('pConsulta').AsString := txtConsulta.Text+'%';
+     end;
+  DM.sqlConsulta.Open;
+end;
+
+procedure TForm1.opcoesClick(Sender: TObject);
+begin
+  if (opcoes.ItemIndex = 0) then
+    begin
+      lblConsulta.Caption := 'Digite o Nome';
+    end;
+  if (opcoes.ItemIndex = 1) then
+    begin
+      lblConsulta.Caption := 'Digite o Bairro';
+    end;
+
+end;
+
+end.
